281b1f9dbf506a262553e9a83bedcd310ac5d455
clarkenciel <clarkenciel@gmail.com>
2017-03-07 19:26:21 -0800

    rewrite for new simple architecture

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # kadfe-client
 Small node js library to talk to the kadfe server.
+All methods return promises
+
+## Methods
++ `status` - Get the status of the coffee.
++ `claim` - Claim the coffee.
--- index.js
+++ index.js
@@ -6,46 +6,28 @@ const server = process.env.KADFE_SERVER
 
 const parse = message => JSON.parse(message)['message']
 
-const getStatus = cb => {
-  request.get(`${server}/status`).
-    on('response', resp => {
-      if (resp.statusCode == 200)
-        cb(parse(message.statusMessage))
-      else
-        cb('Could not get coffee data at this time')
-    }).
-    on('error', err => {
-      console.log(err)
-      cb(err)
-    })
+const coffeeEP = `${server}/coffee`
+
+const status = () => {
+  return new Promise((resolve, error) => 
+    request.get(coffeeEP,
+      (err, resp, bod) => {
+        if (err) error(err)
+        else if (resp.statusCode !== 200) error(body)
+        else resolve(body) 
+      })
+  )
 }
 
-const claim = (user, cb) => {
-  request.post(`${server}/claim?user=${user}`).
-    on('response', resp => {
-      if (resp.statusCode == 200)
-        cb(parse(message.statusMessage))
-      else
-        cb(`Could not claim coffee for ${user}`)
-    }).
-    on('error', err => {
-      console.log(err)
-      cb(err)
-    })
+const claim = () => {
+  return new Promise((resolve, error) =>
+    request.post(coffeeEP,
+      (err, resp, bod) => {
+        if (err) error(err)
+        else if (resp.statusCode !== 200) error(body)
+        else resolve(body)
+      })
+  )
 }
 
-const release = (user, cb) => {
-  request.post(`${server}/release?user=${user}`).
-    on('response', resp => {
-      if (resp.statusCode == 200)
-        cb(parse(message.statusMessage))
-      else
-        cb(`Could not release coffee for ${user}`)
-    }).
-    on('error', err => {
-      console.log(err)
-      cb(err)
-    })
-}
-
-module.exports = { getStatus, claim, release }
+module.export = { status, claim }
