c750427a67e57d2e4156eee44e9e238e0aab53b1
clarkenciel <clarkenciel@gmail.com>
2017-04-04 19:32:48 -0700

    bumping version adding exceptions for env vars

--- index.js
+++ index.js
@@ -6,15 +6,62 @@ const WebSocket = require('ws')
 // server address setup
 const host = process.env.KADFE_HOST
 const port = process.env.KADFE_PORT ? `:${process.env.KADFE_PORT}` : ''
-const ssl = JSON.parse(process.env.KADFE_SSL) ? 's' : ''
+const ssl = process.env.KADFE_SSL &&
+  (JSON.parse(process.env.KADFE_SSL) ? 's' : '')
+
+// validate our environment variables
+if (!host || !ssl) {
+  throw(Error("Please provide the KADFE_HOST and KADFE_SSL environment variables"))
+}
+
+// construct our server url
 const server = `http${ssl}://${host}${port}`
 
 const parse = message => JSON.parse(message)['message']
-
 const coffeeEP = `${server}/coffee`
 const coffeeWS = `ws${ssl}://${host}`
 
-// Returns promise of WebSockets to come
+
+// Interface
+/* Fetch the current coffee status from the server */
+const coffeeStatus = () => {
+  return new Promise((resolve, error) =>
+    request.get(coffeeEP,
+      (err, resp, bod) => {
+        if (err) error(err)
+        else if (resp.statusCode !== 200) error(bod)
+        else resolve(JSON.parse(bod))
+      })
+  )
+}
+
+/* "Clear" the current coffee status from the server */
+const clearCoffee = () => {
+  return new Promise((resolve, error) =>
+    request.delete(coffeeEP,
+      (err, resp, bod) => {
+        if (err) error(err)
+        else if (resp.statusCode !== 200) error(bod)
+        else resolve(JSON.parse(bod))
+      })
+  )
+}
+
+/* Update the server to know that coffee exists */
+const makeCoffee = () => {
+  return new Promise((resolve, error) =>
+    request.post(coffeeEP,
+      (err, resp, bod) => {
+        if (err) error(err)
+        else if (resp.statusCode !== 200) error(bod)
+        else resolve(JSON.parse(bod))
+      })
+  )
+}
+
+/* Return a promise that will contain a websocket connection
+ * to the server if one can be created.
+ */
 const openSocket = () => {
   return new Promise((resolve, error) => {
     var ws = new WebSocket(coffeeWS, {
@@ -29,40 +76,6 @@ const openSocket = () => {
     })
     resolve(ws)
   })
-}
-
-// Interace
-const coffeeStatus = () => {
-  return new Promise((resolve, error) =>
-    request.get(coffeeEP,
-      (err, resp, bod) => {
-        if (err) error(err)
-        else if (resp.statusCode !== 200) error(bod)
-        else resolve(JSON.parse(bod))
-      })
-  )
-}
-
-const clearCoffee = () => {
-  return new Promise((resolve, error) =>
-    request.delete(coffeeEP,
-      (err, resp, bod) => {
-        if (err) error(err)
-        else if (resp.statusCode !== 200) error(bod)
-        else resolve(JSON.parse(bod))
-      })
-  )
-}
-
-const makeCoffee = () => {
-  return new Promise((resolve, error) =>
-    request.post(coffeeEP,
-      (err, resp, bod) => {
-        if (err) error(err)
-        else if (resp.statusCode !== 200) error(bod)
-        else resolve(JSON.parse(bod))
-      })
-  )
 }
 
 module.exports = { openSocket, coffeeStatus, clearCoffee, makeCoffee }
